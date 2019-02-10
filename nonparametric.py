5f49393337c9f22ff8ee41f506a7ea251fa72afc
Sergey <xxsmotur@gmail.com>
2019-02-10 17:39:06 +0500

    implementation of Mann-Whitney U Test

--- nonparametric.py
+++ nonparametric.py
@@ -1,6 +1,6 @@
-from numpy.random import seed
-from numpy.random import randn
+from numpy.random import seed, randn
 import numpy as np
+from scipy.stats import mannwhitneyu
 
 
 class Dataset:
@@ -8,12 +8,12 @@ class Dataset:
     numpy array
     """
     def __init__(self, dataset):
-        self._dataset = dataset
+        self.dataset = dataset
     
     def mean(self):
         """ return mean from dataset
         """
-        return np.mean(self._dataset)
+        return np.mean(self.dataset)
     
     def __len__(self):
         """ returns length of array
@@ -23,5 +23,15 @@ class Dataset:
     def std(self):
         """ return standard deviation of the dataset
         """
-        return np.std(self._dataset)
+        return np.std(self.dataset)
+
+def mann_whitney_u(ds1, ds2, alpha=0.05):
+    """ this method return True if this is same
+    distribution(fail to reject H0) and False if this different distributions
+    """
+    if not isinstance(ds1, Dataset) or not isinstance(ds2, Dataset):
+        raise Exception("input data not is Dataset type")
+    stat, p = mannwhitneyu(data1, data2)
+    return True if p > alpha else False
+    
     
